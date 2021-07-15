e1be2f7e83433b416e32cbf49ccd7dc1932f1880
chaoqi <qichao07@gmail.com>
2021-07-14 22:11:15 -0400

    commit hello servlet

--- src/main/java/com/laioffer/jupiter/GameServlet.java
+++ src/main/java/com/laioffer/jupiter/GameServlet.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class GameServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().print("Hello World");
+        response.getWriter().print("Hello World!");
     }
 
     @Override
