e218422b0df65e6e4e2c6ae0248d7dbe97e81b47
chaoqi <qichao07@gmail.com>
2021-07-14 23:15:40 -0400

    commit hello servlet

--- src/main/java/com/laioffer/jupiter/GameServlet.java
+++ src/main/java/com/laioffer/jupiter/GameServlet.java
@@ -1,5 +1,7 @@
 package com.laioffer.jupiter;
 
+import org.json.JSONObject;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -9,7 +11,14 @@ import java.io.IOException;
 public class GameServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().print("Hello World!!!");
+        response.setContentType("application/json");
+        JSONObject game = new JSONObject();
+        game.put("name", "wold of warcraft");
+        game.put("developer", "Blizzard Enterainment");
+        game.put("release_time", "Feb 11, 2005");
+        game.put("website", "https://www.worldofwarcraft.com");
+        game.put("price", 49.99);
+        response.getWriter().println(game);
     }
 
     @Override
