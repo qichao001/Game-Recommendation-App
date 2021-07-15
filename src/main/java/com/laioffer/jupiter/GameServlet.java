13f9eb40e52ee2d7a03f847a30e0cfe899cc4b48
chaoqi <qichao07@gmail.com>
2021-07-15 00:31:26 -0400

    commit hello servlet

--- src/main/java/com/laioffer/jupiter/GameServlet.java
+++ src/main/java/com/laioffer/jupiter/GameServlet.java
@@ -1,28 +1,61 @@
 package com.laioffer.jupiter;
 
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.laioffer.jupiter.entity.Game;
+import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(name = "GameServlet", urlPatterns = "/game")
 public class GameServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        response.setContentType("application/json");
+//        JSONObject game = new JSONObject();
+//        game.put("name", "wold of warcraft");
+//        game.put("developer", "Blizzard Enterainment");
+//        game.put("release_time", "Feb 11, 2005");
+//        game.put("website", "https://www.worldofwarcraft.com");
+//        game.put("price", 49.99);
+//        response.getWriter().println(game);
         response.setContentType("application/json");
-        JSONObject game = new JSONObject();
-        game.put("name", "wold of warcraft");
-        game.put("developer", "Blizzard Enterainment");
-        game.put("release_time", "Feb 11, 2005");
-        game.put("website", "https://www.worldofwarcraft.com");
-        game.put("price", 49.99);
-        response.getWriter().println(game);
+        ObjectMapper mapper = new ObjectMapper();
+        Game.Builder builder = new Game.Builder();
+        builder.setName("World of Warcraft");
+        builder.setDeveloper("Blizzard Entertainment");
+        builder.setReleaseTime("Feb 20, 2005");
+        builder.setWebsite("https://www.worldofwarcraft.com");
+        builder.setPrice(49.99);
+
+        Game game = builder.build();
+        response.getWriter().print(mapper.writeValueAsString(game));
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        JSONObject jsonRequest = new JSONObject(IOUtils.toString(request.getReader()));
+        String name = jsonRequest.getString("name");
+        String developer = jsonRequest.getString("developer");
+        String releaseTime = jsonRequest.getString("release_time");
+        String website = jsonRequest.getString("website");
+        float price = jsonRequest.getFloat("price");
 
+        System.out.println("Name is: " + name);
+        System.out.println("Developer is: " + developer);
+        System.out.println("Release time is: " + releaseTime);
+        System.out.println("Website is: " + website);
+        System.out.println("Price is: " + price);
+
+        response.setContentType("application/json");
+        JSONObject jsonResponse = new JSONObject();
+        jsonResponse.put("status", "ok");
+        response.getWriter().println(jsonResponse);
     }
 }
